6f6fbfbd21c0b79ec9fadfa6cf97bb50b26f3148
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-02-02 03:33:20 -0500

    DRY code to print CIDRs to disk

--- getips.py
+++ getips.py
@@ -4,19 +4,24 @@
 import json
 import urllib.request
 
+
+def output_cidrs(filename: str, cidrs: list[str]):
+    """Output the given CIDRs to the given file."""
+    with open(filename, "w", encoding="utf-8") as out:
+        for cidr in cidrs:
+            out.write(f"{cidr}\n")
+
+
 with urllib.request.urlopen("https://ip-ranges.amazonaws.com/ip-ranges.json") as url:
     data = json.loads(url.read().decode())
     # Print all AWS IPs
     allawsips = list(ip_prefix["ip_prefix"] for ip_prefix in data["prefixes"])
-    with open("awsips.txt", "w") as f:
-        for cidr in allawsips:
-            f.write("%s\n" % cidr)
+    output_cidrs("awsips.txt", allawsips)
+
     # Print all Cloudfront IPs
     cloudfront = list(
         ip_prefix["ip_prefix"]
         for ip_prefix in data["prefixes"]
         if "CLOUDFRONT" in ip_prefix["service"]
     )
-    with open("cloudfront.txt", "w") as f:
-        for cidr in cloudfront:
-            f.write("%s\n" % cidr)
+    output_cidrs("cloudfront.txt", cloudfront)
