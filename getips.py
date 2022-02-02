02f2dfbcdadee5b0bca1b2f70d4f7a7ba64e53c3
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-02-02 03:26:46 -0500

    Run black and isort against script

--- getips.py
+++ getips.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
-import urllib.request
+# Standard Python Libraries
 import json
+import urllib.request
 
 with urllib.request.urlopen("https://ip-ranges.amazonaws.com/ip-ranges.json") as url:
     data = json.loads(url.read().decode())
     # Print all AWS IPs
-    allawsips= list(ip_prefix["ip_prefix"] for ip_prefix in data["prefixes"])
-    with open('awsips.txt', 'w') as f:
+    allawsips = list(ip_prefix["ip_prefix"] for ip_prefix in data["prefixes"])
+    with open("awsips.txt", "w") as f:
         for cidr in allawsips:
             f.write("%s\n" % cidr)
     # Print all Cloudfront IPs
-    cloudfront = list(ip_prefix["ip_prefix"] for ip_prefix in data["prefixes"] if "CLOUDFRONT" in ip_prefix["service"])
-    with open('cloudfront.txt', 'w') as f:
+    cloudfront = list(
+        ip_prefix["ip_prefix"]
+        for ip_prefix in data["prefixes"]
+        if "CLOUDFRONT" in ip_prefix["service"]
+    )
+    with open("cloudfront.txt", "w") as f:
         for cidr in cloudfront:
             f.write("%s\n" % cidr)
